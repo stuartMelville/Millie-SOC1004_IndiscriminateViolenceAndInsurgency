5c51dc507d9ea594102ce304d30b82911f98bb7a
stuart.alan.melville@gmail.com <stuart.alan.melville@gmail.com>
2024-12-01 15:59:39 +0000

    Adding more sprintf for results

--- scripts/script.R
+++ scripts/script.R
@@ -18,6 +18,7 @@ villagesRussiaAttacked <- chechen %>%
 nrow(villagesRussiaAttacked)
 count(villagesRussiaAttacked)
 numberOfVillagesRussiaAttacked <- nrow(villagesRussiaAttacked)
+
 ###################################
 # Part 2                          #
 # Number of villages Not attacked #
@@ -33,29 +34,28 @@ nrow(villagesRussiaDidNotAttack) + nrow(villagesRussiaAttacked)
 numberOfVillagesInChechen <- nrow(chechen)
 sprintf("Out of %d villages, Russia attacked %d and did not attack %d.", nrow(chechen),nrow(villagesRussiaAttacked),  nrow(villagesRussiaDidNotAttack))
 sprintf("Out of %d villages, Russia attacked %d and did not attack %d.", numberOfVillagesInChechen,numberOfVillagesRussiaAttacked,  numberOfVillagesRussiaDidNotAttack)
-
+rm(villagesRussiaAttacked)
+rm(numberOfVillagesRussiaAttacked)
+rm(villagesRussiaDidNotAttack)
+rm(numberOfVillagesRussiaDidNotAttack)
+rm(numberOfVillagesInChechen)
 ##############################################################################
 # 1.2. Did Russian artillery result in a greater number of deaths in Groznyy #
 # compared to the villages outside of Groznyy? Conduct the comparison        #
 # in terms of the mean and median. (10 points)                               #
 ##############################################################################
-
 # artilleryDeathsInGrozny dplyr way 
 artilleryDeathsInGrozny <- chechen %>% 
   filter(groznyy == 1 & fire == 1 & deaths > 0)
 
 meanArtilleryDeathsInGrozny <- mean(artilleryDeathsInGrozny$deaths, na.rm = T)
-meanArtilleryDeathsInGrozny
 medianArtilleryDeathsInGrozny <- median(artilleryDeathsInGrozny$deaths, na.rm = T)
-medianArtilleryDeathsInGrozny
 
 # artilleryDeathsNotInGrozny dplyr way 
 artilleryDeathsNotInGrozny <- chechen %>% 
   filter(groznyy == 0  & fire == 1 &   deaths > 0)
 meanArtilleryDeathsNotInGrozny <- mean(artilleryDeathsNotInGrozny$deaths, na.rm = T)
-meanArtilleryDeathsNotInGrozny
 medianArtilleryDeathsNotInGrozny <- median(artilleryDeathsNotInGrozny$deaths, na.rm = T)
-medianArtilleryDeathsNotInGrozny
 
 # artilleryDeathsInGrozny  R way 
 mean(chechen$deaths[chechen$groznyy == 1 & chechen$fire == 1 & chechen$deaths > 0], na.rm = T)
@@ -65,17 +65,32 @@ median(chechen$deaths[chechen$groznyy == 1 & chechen$fire == 1 & chechen$deaths 
 mean(chechen$deaths[chechen$groznyy == 0 & chechen$fire == 1 & chechen$deaths > 0], na.rm = T)
 median(chechen$deaths[chechen$groznyy == 0 & chechen$fire == 1 & chechen$deaths > 0], na.rm = T)
 
-if(meanArtilleryDeathsInGrozny > meanArtilleryDeathsNotInGrozny){
-  meanDeathsInGroznyyGreaterOrLesser <- "greater"
-}else{
-  meanDeathsInGroznyyGreaterOrLesser <- "lesser"  
+# Function which returns "greater" or "lesser" based on two numbers
+greaterOrLesser <- function(first,second){
+  if(first > second){
+    return ("greater")
+  }else{
+    return("lesser")
+  }
 }
-if(medianArtilleryDeathsInGrozny > medianArtilleryDeathsNotInGrozny){
-  medianDeathsInGroznyyGreaterOrLesser <- "greater"
-}else{
-  medianDeathsInGroznyyGreaterOrLesser <- "lesser"  
-}
+
+meanDeathsInGroznyyGreaterOrLesser <- greaterOrLesser(meanArtilleryDeathsInGrozny, meanArtilleryDeathsNotInGrozny)
+medianDeathsInGroznyyGreaterOrLesser <- greaterOrLesser(medianArtilleryDeathsInGrozny, medianArtilleryDeathsNotInGrozny)
+
 sprintf("Based on the mean average, Russian artillery resulted in a %s number of Deaths in Groznyy (%f) compared to outside of Grozny (%f)", meanDeathsInGroznyyGreaterOrLesser,meanArtilleryDeathsInGrozny,meanArtilleryDeathsNotInGrozny)
 sprintf("and based on the median average, Russian artillery resulted in a %s number of Deaths in Groznyy (%f) compared to outside of Grozny (%f)", medianDeathsInGroznyyGreaterOrLesser,medianArtilleryDeathsInGrozny,medianArtilleryDeathsNotInGrozny)
 
+rm(artilleryDeathsInGrozny)
+rm(meanArtilleryDeathsInGrozny)
+rm(medianArtilleryDeathsInGrozny)
+rm(artilleryDeathsNotInGrozny)
+rm(meanArtilleryDeathsNotInGrozny)
+rm(medianArtilleryDeathsNotInGrozny)
+rm(greaterOrLesser)
+rm(meanDeathsInGroznyyGreaterOrLesser)
+rm(medianDeathsInGroznyyGreaterOrLesser)
+
+
+####################################
+rm(chechen)
 
